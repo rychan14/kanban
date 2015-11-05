aae64bf23012fe65992b9946051411fa6f744d09
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-04 19:54:56 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var copy = require('gulp-copy');
+var webserver =  require('gulp-webserver');
+
+gulp.task('copy', function() {
+    gulp.src('src/index.html')
+        .pipe(copy('build', {
+            prefix: 1
+        }));
+});
+
+gulp.task('babel', function() {
+    browserify('src/scripts/kanban.jsx')
+        .transform(babelify, {presets: ["es2015", "react"]})
+        .bundle()
+        .on('error', function(error) {
+            console.log(error.message);
+        })
+        .pipe(source('kanban.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', function() {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            file: 'kanban.scss'
+        })
+        .on('error', sass.logError))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/index.html', ['copy']);
+    gulp.watch('src/scripts/**/*.jsx', ['babel']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('server', function() {
+  gulp.src('build')
+    .pipe(webserver({
+      livereload: true,
+      fallback: 'index.html',
+      open: true
+    }));
+});
+
+gulp.task('default', ['copy', 'babel', 'sass', 'watch', 'server']);
