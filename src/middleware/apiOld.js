e4527722b4de68ada8ebc88359410e9d84dcfc9f
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-08 20:19:38 -0800

    Rewrote api middleware. it doesnt work... yet.

--- src/middleware/apiOld.js
+++ src/middleware/apiOld.js
@@ -1,8 +1,25 @@
 import {Schema, arrayOf, normalize} from 'normalizr';
-import request from 'browser-request';
 import {camelizeKeys} from 'humps';
 
+
 const API_ROOT = 'https://api.github.com/';
+
+function callApi(endpoint, schema) {
+  const fullUrl =  (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
+
+  return fetch(fullUrl)
+  .then(response =>
+    response.json().then(json => ({ json, response }))
+  ).then(({ json, response }) => {
+    if (!response.ok) {
+      return Promise.reject(json);
+    }
+
+    const camelizedJson = camelizeKeys(json);
+
+    return normalize(camelizedJson, schema);
+  });
+}
 
 const repoSchema = new Schema('repos', {
   idAttribute: 'fullName'
@@ -26,21 +43,14 @@ export const Schemas = {
 export const CALL_API = Symbol('Call Api');
 
 export default store => next => action => {
-  const callApi = action[CALL_API];
+  const callAPI = action[CALL_API];
 
-  if(typeof callApi === 'undefined') {
+  if (typeof callAPI === 'undefined') {
     return next(action);
   }
 
-  let {endpoint} = callApi;
-  const {schema, types} = callApi;
-  const [requestType, successType, failureType] = types;
-
-  function actionWith(data) {
-    const finalAction = Object.assign({}, action, data);
-    delete finalAction[CALL_API];
-    return finalAction;
-  }
+  let{endpoint} = callAPI;
+  const{schema, types} = callAPI;
 
   if(typeof endpoint === 'function') {
     endpoint = endpoint(store.getState());
@@ -62,33 +72,24 @@ export default store => next => action => {
     throw new Error('Expected action types to be strings.');
   }
 
+  function actionWith(data) {
+    const finalAction = Object.assign({}, action, data);
+    delete finalAction[CALL_API];
+    return finalAction;
+  }
+
+  const [requestType, successType, failureType] = types;
   next(actionWith({type: requestType}));
 
   return callApi(endpoint, schema)
-    .then(response => next(actionWith({
+    .then(
+    response => next(actionWith({
       response,
-      type: successtype
-    })))
-    .catch(error => next(actionWith({
+      type: successType
+    })),
+    error => next(actionWith({
       type: failureType,
       error: error.message || 'Something bad happened.'
-    })));
+    }))
+  );
 };
-
-function callApi(endpoint, schema) {
-  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
-
-  return new Promise((resolve, reject) => {
-    request(fullUrl, (error, response, body) => {
-      if(!error && response.statusCode === 200) {
-        const camelizedJson = camelize(body);
-        return resolve(normalize(camelizedJson, schema));
-      }
-      else if(error) {
-        return reject(error);
-      }
-
-      return reject(new Error(`Status code: ${reponse.statusCode}`));
-    });
-  });
-}
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import {devTools, persistState} from 'redux-devtools';
-import thunk from 'redux-thunk';
 import api from '../middleware/api';
 import rootReducer from '../reducers';
 
 const finalCreateStore = compose(
-  applyMiddleware(thunk, api),
+  applyMiddleware(api),
   devTools(),
   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
