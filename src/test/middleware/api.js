d922cce08e040c36d6005ef2d2a6a71fa240453d
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-16 17:30:05 -0800

    API tests are running

--- src/test/middleware/api.js
+++ src/test/middleware/api.js
@@ -1,9 +1,13 @@
 import should from 'should';
 import sinon from 'sinon';
 import request from 'browser-request';
-import {callApi, Schemas} from '../../middleware/api';
+import {applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import api, {CALL_API, callApi, Schemas} from '../../middleware/api';
+import {loadRepo, REPO_REQUEST, REPO_SUCCESS, REPO_FAILURE} from '../../actions/repos';
 import {RepoObj} from '../data/api';
 
+const middlewares = [thunk, api]
 
 describe('API Middleware', () => {
   describe('Call Api Function', () => {
@@ -83,4 +87,97 @@ describe('API Middleware', () => {
       });
     });
   });
+
+  describe('Api Middleware Default Function', () => {
+    let repoAction;
+
+    before(done => {
+      repoAction = {
+        [CALL_API]: {
+          types: [REPO_REQUEST, REPO_SUCCESS, REPO_FAILURE],
+          endpoint: 'repos/mrarnoldpalmer/kanban',
+          schema: Schemas.REPO
+        }
+      };
+      done();
+    });
+
+    describe('Middleware Function Called With CALL_API Action', () => {
+      describe('Call Api Successful', () => {
+        before(done => {
+          sinon.stub(request, 'get')
+            .yields(null, {statusCode: 200}, JSON.stringify(RepoObj));
+          done();
+        });
+
+        after(done => {
+          request.get.restore();
+          done();
+        });
+
+        it('Should return action REPO_REQUEST and REPO_SUCCESS Actions', done => {
+          const expectedActions = [
+            {type: REPO_REQUEST},
+            {type: REPO_SUCCESS}
+          ];
+          const store = mockStore({}, expectedActions, done)
+          store.dispatch(repoAction);
+        });
+      });
+
+      describe('Call Api Failed', () => {
+        before(done => {
+          sinon.stub(request, 'get')
+            .yields({message: 'Error Message'});
+          done();
+        });
+
+        after(done => {
+          request.get.restore();
+          done();
+        });
+
+        it('Should return action REPO_REQUEST and REPO_FAILURE', done => {
+          const expectedActions = [
+            {type: REPO_REQUEST},
+            {type: REPO_FAILURE}
+          ];
+          const store = mockStore({}, expectedActions, done)
+          store.dispatch(repoAction);
+        });
+      });
+    });
+  });
 });
+
+function mockStore(getState, expectedActions, done) {
+  function mockStoreWithoutMiddleware() {
+    return {
+      getState() {
+        return typeof getState === 'function' ?
+          getState() :
+          getState
+      },
+
+      dispatch(action) {
+        const expectedAction = expectedActions.shift()
+
+        try {
+          action.should.equal.expectedAction;
+          if (done && !expectedActions.length) {
+            done()
+          }
+          return action
+        } catch (e) {
+          done(e)
+        }
+      }
+    }
+  }
+
+  const mockStoreWithMiddleware = applyMiddleware(
+    ...middlewares
+  )(mockStoreWithoutMiddleware);
+
+  return mockStoreWithMiddleware();
+}
