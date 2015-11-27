50d3fa2dfb50e4771b598118029a7feffb347a90
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-27 15:38:59 -0800

    Issue rendering. Some basic Radium styles added for fun

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import Radium from 'radium';
 
 export default class Issue extends Component {
   render() {
     const id = Object.keys(this.props.issue)[0];
     const issue = this.props.issue[id];
+    const styles = {
+      base: {
+        borderRadius: 4,
+        boxShadow: '5px 5px 5px 5px #D8D8D8',
+        width: '500px',
+        height: '250px'
+      }
+    };
     return (
-      <div>
+      <div style={styles.base}>
         <h1>
           Title: {issue.title}
         </h1>
@@ -16,3 +25,5 @@ export default class Issue extends Component {
     );
   }
 }
+
+Issue = Radium(Issue);
