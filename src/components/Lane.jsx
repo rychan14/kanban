064ab81c974835a0c06086474eeb419449719e13
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-11-28 17:18:29 -0800

    Added lane component for the future.

---
+++ src/components/Lane.jsx
@@ -0,0 +1,12 @@
+import React, {Component, PropTypes} from 'react';
+import Radium from 'radium';
+
+export default class Lane extends Component {
+  render() {
+    return (
+      <div>
+        Lane Component
+      </div>
+    );
+  }
+}
