ae06455d44e2014b7f54288f0c7cf4f3861c3c12
Chan, Ryan <rychan14@gmail.com>
2016-02-11 17:10:57 -0800

    got rid of thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import {devTools, persistState} from 'redux-devtools';
-import thunk from 'redux-thunk';
 import api from '../middleware/api';
 import issues from '../middleware/issues';
 import rootReducer from '../reducers';
 
 const finalCreateStore = compose(
-  applyMiddleware(thunk, api, issues),
+  applyMiddleware(api, issues),
   devTools(),
   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
